a7579080050548d3ce233b1a571f8ddd936e1e33
loganfortune <logan.fortune@orange.fr>
2020-02-19 09:20:05 +0100

    improvement gnuplot plots

--- maths.py
+++ maths.py
@@ -27,25 +27,28 @@ def test_tisean_package_service_PartOne():
     print("We are testing the Henon map.")
     gp.c('set yrange [-1:1]')
     gp.c('plot \'< henon -B0 -A2. -l100\' using 0:1 with linespoints') # a = 2.0, b=0 , 100 iterations
-    gp.p('./Tisean_test_accuracy/part1_henonmap.ps')
+    gp.pdf('./Tisean_test_accuracy/part1_henonmap.ps')
     
     # Bifurcation Diagram
     print("Bifurcation Henon Map.")
-    gp.c('set autoscale') # Important line because if you have plotted something before the next plot might be impacted !
-    gp.c('plot \'< henon -B0 -A2. -l50000 | delay\' with dots') # a = 2.0, b = 0, 50000 iterations
+    gp.c('reset session')
+    gp.c('load \'./Tisean_test_accuracy/henon.gnu\'')
     gp.p('./Tisean_test_accuracy/part1_henonmap_bifurcation.ps')
+    gp.c('reset session') # Important line because if you have plotted something before the next plot might be impacted !
+    gp.c('plot \'< henon -B0 -A2. -l50000 | delay\' with dots') # a = 2.0, b = 0, 50000 iterations
+    gp.p('./Tisean_test_accuracy/part1_henonmap_delay.ps')
     
     # Histogram
     print("Histogram.")
     os.system("henon -l10000 -B0 -A2. -o ./Tisean_test_accuracy/henon.dat") # a = 2.0, b = 0, 10000 iterations
-    gp.c('set autoscale')
+    gp.c('reset session')
     gp.c('plot "< histogram -b50 ./Tisean_test_accuracy/henon.dat" with boxes') # 50 bins
     gp.p('./Tisean_test_accuracy/histogram.ps')
     
     # Lyapunov
     print("Lyapunov.")
     os.system('henon -l10000 -B0 -A2. | lyap_r -s20 -o ./Tisean_test_accuracy/lyap_r_henon.dat') # a = 2.0, b = 0, 10000 iterations for the Henon map, 20 iterations for the Lyapunov Exponent
-    gp.c('set autoscale')
+    gp.c('reset session')
     gp.c('plot \'./Tisean_test_accuracy/lyap_r_henon.dat\' with lines, x*log(2.)-8, -log(2.), \'lyap_r.dat\' with lines ')
     gp.p('./Tisean_test_accuracy/Lyapunov_henon.ps')
     
@@ -67,7 +70,7 @@ def test_tisean_package_service_PartTwo():
     
     # Histogram
     print("Histogram.")
-    gp.c('set autoscale')
+    gp.c('reset session')
     gp.c('plot "< histogram -b50 ./Tisean_test_accuracy/amplitude.dat" with boxes')
     gp.p('./Tisean_test_accuracy/histogram_amplitude.ps')
     
@@ -80,6 +83,7 @@ def test_tisean_package_service_PartTwo():
     stream.close()
     
     #Spectrum
+    gp.c('reset session')
     os.system("spectrum ./Tisean_test_accuracy/amplitude.dat -o ./Tisean_test_accuracy/amplitude.dat_sp ")
     gp.c('set logscale y')
     gp.c('plot \'amplitude.dat_sp\'')
