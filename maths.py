45387d1c0eedbb30d3ec5cd930f1bd7a784757a5
loganfortune <logan.fortune@orange.fr>
2020-02-19 11:16:51 +0100

    windows png

--- maths.py
+++ maths.py
@@ -25,32 +25,37 @@ def test_tisean_package_service_PartOne():
     
     # Input Data
     print("We are testing the Henon map.")
+    gp.c('set terminal png size 400,300')
+    gp.c('set output \'./Tisean_test_accuracy/part1_henonmap.png\'')
     gp.c('set yrange [-1:1]')
     gp.c('plot \'< henon -B0 -A2. -l100\' using 0:1 with linespoints') # a = 2.0, b=0 , 100 iterations
-    gp.pdf('./Tisean_test_accuracy/part1_henonmap.ps')
-    
+
     # Bifurcation Diagram
     print("Bifurcation Henon Map.")
     gp.c('reset session')
+    gp.c('set terminal png size 400,300')
+    gp.c('set output \'./Tisean_test_accuracy/part1_henonmap_bifurcation.png\'')
     gp.c('load \'./Tisean_test_accuracy/henon.gnu\'')
-    gp.p('./Tisean_test_accuracy/part1_henonmap_bifurcation.ps')
     gp.c('reset session') # Important line because if you have plotted something before the next plot might be impacted !
+    gp.c('set terminal png size 400,300')
+    gp.c('set output \'./Tisean_test_accuracy/part1_henonmap_delay.png\'')
     gp.c('plot \'< henon -B0 -A2. -l50000 | delay\' with dots') # a = 2.0, b = 0, 50000 iterations
-    gp.p('./Tisean_test_accuracy/part1_henonmap_delay.ps')
     
     # Histogram
     print("Histogram.")
     os.system("henon -l10000 -B0 -A2. -o ./Tisean_test_accuracy/henon.dat") # a = 2.0, b = 0, 10000 iterations
     gp.c('reset session')
+    gp.c('set terminal png size 400,300')
+    gp.c('set output \'./Tisean_test_accuracy/part1_histogram.png\'')
     gp.c('plot "< histogram -b50 ./Tisean_test_accuracy/henon.dat" with boxes') # 50 bins
-    gp.p('./Tisean_test_accuracy/histogram.ps')
     
     # Lyapunov
     print("Lyapunov.")
     os.system('henon -l10000 -B0 -A2. | lyap_r -s20 -o ./Tisean_test_accuracy/lyap_r_henon.dat') # a = 2.0, b = 0, 10000 iterations for the Henon map, 20 iterations for the Lyapunov Exponent
     gp.c('reset session')
+    gp.c('set terminal png size 400,300')
+    gp.c('set output \'./Tisean_test_accuracy/Lyapunov_henon.png\'')
     gp.c('plot \'./Tisean_test_accuracy/lyap_r_henon.dat\' with lines, x*log(2.)-8, -log(2.), \'lyap_r.dat\' with lines ')
-    gp.p('./Tisean_test_accuracy/Lyapunov_henon.ps')
     
     print("Finished. See Tisean_test_accuracy directory !")
     
@@ -71,8 +76,9 @@ def test_tisean_package_service_PartTwo():
     # Histogram
     print("Histogram.")
     gp.c('reset session')
+    gp.c('set terminal png size 400,300')
+    gp.c('set output \'./Tisean_test_accuracy/histogram_amplitude.png\'')
     gp.c('plot "< histogram -b50 ./Tisean_test_accuracy/amplitude.dat" with boxes')
-    gp.p('./Tisean_test_accuracy/histogram_amplitude.ps')
     
     # Correlation
     stream = os.popen('corr -D10 ./Tisean_test_accuracy/amplitude.dat') # Only 10 correlations values : we need to see it crossing zero...
@@ -84,10 +90,11 @@ def test_tisean_package_service_PartTwo():
     
     #Spectrum
     gp.c('reset session')
+    gp.c('set terminal png size 400,300')
+    gp.c('set output \'./Tisean_test_accuracy/part2_amplitude_spectrum.png\'')
     os.system("spectrum ./Tisean_test_accuracy/amplitude.dat -o ./Tisean_test_accuracy/amplitude.dat_sp ")
     gp.c('set logscale y')
     gp.c('plot \'amplitude.dat_sp\'')
-    gp.p('./Tisean_test_accuracy/part2_amplitude_spectrum.ps')
     
 test_tisean_package_service_PartOne()
 test_tisean_package_service_PartTwo()
